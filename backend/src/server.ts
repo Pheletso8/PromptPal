cbc3aa0f2eb588b9e166f84264aacea940f18f59
Pheletso8 <pheletso.marumoloe8@gmail.com>
2026-03-19 15:08:09 +0200

    Added fixes to deploy the backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,7 +14,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://prompt-pal-six.vercel.app/'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
@@ -37,7 +40,7 @@ app.get('/api/status', (req, resentment) => {
 // Start Server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  
+
   // Database Connection (asynchronous)
   mongoose.connect(process.env.MONGO_URI as string)
     .then(() => {
